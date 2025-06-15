b9c430e26c05e877ebd0651a5c729c556c92108c
Byeongryul Cho <byeongryul.cho@gmail.com>
2025-06-15 18:51:44 +0900

    neo-tree hidden file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,18 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_by_name = {
+            '.DS_Store',
+            '.git',
+          }
+        }
+      }
     }
 }
 
